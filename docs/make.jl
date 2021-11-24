eed30a5d2d462646081b70abffc08e1ddbaebe1e
Fhoeddinghaus <info@xefli.de>
2021-11-24 21:06:28 +0100

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -3,7 +3,3 @@ push!(LOAD_PATH,"../src/")
 using Documenter, ClosedGroupFunctions
 
 makedocs(sitename="ClosedGroupFunctions.jl")
-
-deploydocs(
-    repo = "github.com/Fhoeddinghaus/ClosedGroupFunctions.jl.git",
-)
